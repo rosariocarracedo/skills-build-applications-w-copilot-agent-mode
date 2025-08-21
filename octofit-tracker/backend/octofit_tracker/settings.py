f69602183c4f868106df8446b860c70d7c7305ed
rosariocarracedo <123625222+rosariocarracedo@users.noreply.github.com>
2025-08-21 09:57:48 +0000

    Update Django settings to restrict ALLOWED_HOSTS and add dynamic API root endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-$j-jz7f!iyo^1c!uf1dn$dt-0^8&qw7*944!=_*&g0z=ihzt_l
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace host
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
